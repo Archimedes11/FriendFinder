eb9e51013375751ed26ae2b0c503ce23b354b025
Adam Wolff <adamkwolff@gmail.com>
2020-10-01 01:12:22 -0500

    finished the logic for the friends.js page

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+    {
+      name: "Ahmed",
+      photo:
+        "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"],
+    },
+    {
+      name: "Jacob Deming",
+      photo:
+        "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"],
+    },
+    {
+      name: "Jeremiah Scanlon",
+      photo:
+        "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"],
+    },
+    {
+      name: "Louis T. Delia",
+      photo:
+        "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"],
+    },
+    {
+      name: "Lou Ritter",
+      photo:
+        "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"],
+    },
+    {
+      name: "Jordan Biason",
+      photo:
+        "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"],
+    },
+  ];
+  
+  module.exports.friends = friends;
